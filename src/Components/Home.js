875ef1d14a8cf15dc6769c70e8ee4fd4b9807755
DeLaCruz26 <dametrik.fick@gmail.com>
2021-04-29 19:16:37 -0400

    styled navbar, added social links, added backgroung image

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import image from '../portfolio-img.jpeg'
 
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <main>
+            <img src={image} alt='Keyboard and coffee' className='absolute object-cover w-ful h-full'/>
+            <section>
+                <h1>Greetings. I'm Dametrik</h1>
+            </section>
+        </main>
     );
 }
 
