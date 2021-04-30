205a3c07c51aecc1303fdb2ce3b6f05656e5daec
DeLaCruz26 <dametrik.fick@gmail.com>
2021-04-30 01:38:09 -0400

    styled home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import image from '../portfolio-img.jpeg'
+import image from '../portfolio-img-2.jpeg'
 
 const Home = () => {
     return (
         <main>
-            <img src={image} alt='Keyboard and coffee' className='absolute object-cover w-ful h-full'/>
-            <section>
-                <h1>Greetings. I'm Dametrik</h1>
+            <img src={image} 
+            alt='developer picture' 
+            className='absolute object-cover w-full h-full'
+            />
+            <section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
+                <h1 className='text-6xl text-black font-bold cursive leading-none lg:leading-snug home-name'
+                >
+                    Hello. I'm Dametrik Fick, I'm a full-stack web developer.
+                </h1>
             </section>
         </main>
     );
