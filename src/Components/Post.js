5f4f341cdc06e07c064fed9683677f6c618ca12f
DeLaCruz26 <dametrik.fick@gmail.com>
2021-04-30 22:19:26 -0400

    fixed typo that caused blog post to not render

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -7,7 +7,7 @@ const Post = () => {
 
     useEffect(() => {
         sanityClient
-            .fetch(`*[_type == "post]{
+            .fetch(`*[_type == "post"]{
                 title, 
                 slug,
                 mainImage{
@@ -25,16 +25,16 @@ const Post = () => {
     return (
         <main className='bg-gray-100 min-h-screen p-12'>
             <section className='container mx-auto'>
-                <h1 className='text-5xl flex justify-center cursive'>Blogs Page</h1>
+                <h1 className='text-5xl flex justify-center cursive'>Blogs</h1>
                 <h2 className='text-lg text-gray-600 flex justify-center mb-12'>Welcome to my blogs page</h2>
                 <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
                     {postData && postData.map((post, index) => (
                     <article>
-                        <Link to={'/post/' + post.slud.current} key={post.slug.current}>
+                        <Link to={'/post/' + post.slug.current} key={post.slug.current}>
                             <span className='block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-gray-400' key={index}>
                                 <img src={post.mainImage.asset.url} alt={post.mainImage.alt} className='w-full h-full rounded-r object-cover absolute'/>
                                 <span className='block relative h-full flex justify-end items-end pr-4 pb-4'>
-                                    <h3 className='text-gray-800 text-lg font-blog px-3 py-4 bd-indigo-700 text-gray-100 bg-opacity-75 rounded'>{post.title}</h3>
+                                    <h3 className='text-white text-lg font-blog px-3 py-4 bd-indigo-700 text-gray-100 bg-opacity-75 rounded'>{post.title}</h3>
                                 </span>
                             </span>
                         </Link>
